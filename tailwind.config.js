f296aabcc2de97a8078967e6c714b404cab5d73f
miidddeeee <michealmhe01@gmail.com>
2024-02-20 11:49:12 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",  
+      xl: "1440px"
+    },
+    extend: {
+      backgroundImage: {
+        'heroImage': "url('img/heroImage.jpg')",
+      },
+      colors: {
+        "transparenBlue": "rgba(33, 101, 227, 0.799)",
+      },
+    },
+  },
+  plugins: [],
+}
+
